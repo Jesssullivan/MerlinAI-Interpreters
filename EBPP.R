95e7646b4d63cf9a4db7a0de29f8448fb8645826
Jess <37297218+Jesssullivan@users.noreply.github.com>
2018-03-19 05:52:08 -0400

    functional with "...madeup_2" test set
    using one "filter" function for both score operations.

--- EBPP.R
+++ EBPP.R
@@ -7,12 +7,11 @@ library(data.table)
 options(stringsAsFactors = FALSE)
 #Main file, ".../WIWA_filtered.txt" (WIWA = alpha banding code for Wilson's Warbler)"
 # wi <- fread(".../WIWA_filtered.txt")
-wi <- fread("subset_test_madeup_starz.txt")
 ##test file : see github repo and README/cloud storage link for test sets, "bad data" files, etc 
-#wi <- fread(".../subset_test_madeup_starz.txt")
+wi <- fread(".../subset_test_madeup_2.txt")
 ##Define when and where using mm-dd and County
-bird_of_interest <- "Wilson's Warbler"
-date_of_interest <- "05-18"
+bird_of_interest <- "Starling"
+date_of_interest <- "01-03"
 county_name <- "Middlesex"
 #date range start of calculations
 date_wiwa <- "1997-05-18"
@@ -45,35 +44,21 @@ mean_tab = 0
 rangenum = 1:numdates
 #function meandate calulates the mean observation count for 1 date in the seq. "date366"
 meandate <- function(p) {
-    observations_for_p <- filter(i, `OBSERVATION DATE` == p)
-    obs_individuals_total <- sum(as.numeric(observations_for_p[,2]))
-    mean_obs <- obs_individuals_total / numdates
+    obs_individuals_total <- filter(i, `OBSERVATION DATE` == p)
+    test <- sum(as.numeric(obs_individuals_total[,2]))
+    mean_obs <- test / numdates
     return(mean_obs)
 }
 #Generated sum, mean, and date col. for each day 
-#Preform a similar function for only the day_of_interest:
-daydate <- function(p) {
-  repeat {
-    {
-      ifelse(as.character(i[1:numdates,1]) == p, {
-        obs_individuals_total <- as.numeric(i[1:numdates,2]) + obs_individuals_total
-      } ,
-      print("working..."))
-    }
-    mean_obs <- obs_individuals_total / numdates
-    break 
-  }
-  return(mean_obs)
-}
 #apply meandate to each day of the year:
 mean_x_temp <- sapply(date366, meandate)
 mean_tab <- data.frame(mean_x_temp[!(mean_x_temp == "<NA>")])
 #apply daydate to our date_of_interest:
-DofI_mean_obs <- sapply(date_of_interest, daydate)
+DofI_mean_obs <- sapply(date_of_interest, meandate)
 ###write the files somewhere:
 #write.csv(mean_tab, "...all_averages.csv")
 #write.csv(DofI_mean_obs, "...day_average.csv")
-max_sp_mean <- max(as.numeric(mean_tab[2,]), na.rm = TRUE)
+max_sp_mean <- max(as.numeric(mean_tab[,1]), na.rm = TRUE)
 mean_DOI <- mean(DofI_mean_obs, na.rm = TRUE)
 #end birb score out of 1000, "possible points" 
 #(makes it easier I think to interpret as a human), normalized:
