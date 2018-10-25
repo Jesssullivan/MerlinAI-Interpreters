bededb94e2d728f542a6465b0ea26a852de4b89b
Jesssullivan <jess@sulliwood.org>
2018-10-25 11:18:09 -0400

    JS update; Location, Date, Sp form

--- FrontEnd/AddQuotes4JS.R
+++ FrontEnd/AddQuotes4JS.R
@@ -1,9 +1,17 @@
-file = "QuotedTaxa.csv"
-t = 4 # col num
+setwd("~/Downloads")
+file = "book1.csv"
 df <- read.csv(file)
+length(df[,1])
 list <- data.frame("output",stringsAsFactors = FALSE)
-for(i in 1:length(list[,t])){
-    concat <- paste('"', df[i,t], '",', sep="")
-    list <- rbind(concat, list)
+i <- 1
+df[3,4]
+for(i in 1:as.numeric(length(df[,1]))){
+  r <- formatC(df[i,4], width = 3, format = "d", flag = "0")
+  ro <- paste('"', df[i,2], df[i,3],r, sep = "")
+  row <- paste(ro, df[i,5], '",', sep = "")
+    list <- rbind(row, list)
+    #print(list)
+#    if (i == 10)
+#      break
 }
-write.csv(list, "output.csv")
+write.csv(list, "output_Ccode5.csv")
