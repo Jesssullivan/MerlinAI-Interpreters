1f3f3d374d4b8b0ae78ec00c11ad3d02573d2551
jesssullivan <Jess@sulliwood.org>
2020-08-26 07:59:46 -0400

    backup

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import CenteredGrid from "./Grid";
+
+
+function App() {
+    const [currentTime, setCurrentTime] = useState(0);
+
+    useEffect(() => {
+        fetch('/time').then(res => res.json()).then(data => {
+            setCurrentTime(data.time);
+        });
+    }, []);
+
+  return (
+      <div className="App">
+          <header>
+                D'egg
+              <p>The current time is {currentTime}.</p>
+          </header>
+      </div>
+  );
+}
+
+export default App;
