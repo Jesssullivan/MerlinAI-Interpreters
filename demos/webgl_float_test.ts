f849d391dde5e9ca18b7807c05ce874f5b4b13e0
jesssullivan <Jess@sulliwood.org>
2020-09-02 00:11:06 -0400

    gl shader test

--- demos/webgl_float_test.ts
+++ demos/webgl_float_test.ts
@@ -1,21 +1,24 @@
 // webgl_float_test.ts
-import {audio_model, audio_utils, AudioRecorder, spectrogram_utils} from '../src/index';
-const revi = require('./test_waveform.json');
+import {audio_model, audio_utils, spectrogram_utils} from '../src/index';
 
-window.MediaRecorder = AudioRecorder;
+// a default, hardcoded waveform snippet to try from different devices:
+const testWaveform = require('./test_waveform.json');
 
-const reviButton = document.getElementById("reviButton") as HTMLButtonElement;
+// `test_array` here stands in for what would usually be a
+//  waveform we just recorded out of a Blob:
+const test_array = new Float32Array(testWaveform.length);
 
-const revi_test = new Float32Array(revi.length);
-    for (let i = 0; i < revi_test.length; i++){
-    revi_test[i] = revi[i];
+for (let i = 0; i < testWaveform.length; i++){
+    test_array[i] = testWaveform[i];
 }
 
+/* tslint:disable:prefer-const */
+const webglButton = document.getElementById("webglButton") as HTMLButtonElement;
+const testButton = document.getElementById("testButton") as HTMLButtonElement;
 const canvas = document.querySelector('.visualizer') as HTMLCanvasElement;
 const mainSection = document.querySelector('.container-fluid') as HTMLDivElement;
-
-/* tslint:disable:prefer-const */
 let imgCrop = document.createElement('img');
+/* tslint:enable:prefer-const */
 
 // Spectrogram Visualization Parameters
 const targetSampleRate = 22050;
@@ -31,7 +34,7 @@ const merlinAudio = new audio_model.MerlinAudioModel(LABELS_URL, MODEL_URL);
 function updateVis() {
 
     // visualize the cropped sample
-    const dbSpec = generateSpectrogram(revi_test); //audio_utils.dBSpectrogram(audioData.waveform, spec_params);
+    const dbSpec = generateSpectrogram(test_array); //audio_utils.dBSpectrogram(audioData.waveform, spec_params);
     const cropped_imageURI = spectrogram_utils.dBSpectrogramToImage(dbSpec, topDB);
 
     // create / update cropped visualization
@@ -49,7 +52,7 @@ function updateVis() {
 
     specCropImage!.appendChild(imgCrop);
 
-    return revi_test;
+    return test_array;
 
 }
 
@@ -58,6 +61,7 @@ async function averageClassifyWaveform(waveform : Float32Array) {
     const result = await merlinAudio.averagePredictV3(waveform, targetSampleRate);
     const labels = result[0] as string[];
     const scores = result[1] as Float32Array;
+
     return [labels, scores];
 
 }
@@ -80,7 +84,33 @@ function generateSpectrogram(waveform : Float32Array) : Float32Array[]{
 
 }
 
-reviButton.onclick = () => {
+webglButton.onclick = () => {
+
+    // benefit of the doubt here, ideally client is on a `proper` browser:
+    const gl = canvas.getContext('webgl');
+
+    // we'll return our findings to a placeholder element:
+    const glSupportHolderEl = document.getElementById('glSupportHolder');
+    while (glSupportHolderEl!.firstChild) {
+        glSupportHolderEl!.removeChild(glSupportHolderEl!.firstChild);
+    }
+
+    const prec = gl!.getShaderPrecisionFormat(gl!.VERTEX_SHADER, gl!.HIGH_FLOAT)!.precision;
+
+    if (prec >= 23) {
+        glSupportHolderEl!.prepend("this device's shader precision is " +
+            prec.toString() +
+            ", and should be able to handle float32 webgl values.");
+
+    } else {
+        glSupportHolderEl!.prepend("this device' shader precision is " + prec.toString() +
+            ", and should may not represent float32 values :(");
+
+    }
+
+};
+
+testButton.onclick = () => {
 
     updateVis();
 
@@ -89,14 +119,18 @@ reviButton.onclick = () => {
         sampleHolderEl!.removeChild(sampleHolderEl!.firstChild);
     }
 
-    averageClassifyWaveform(revi_test).then(([labels, scores]) => {
+    averageClassifyWaveform(test_array).then(([labels, scores]) => {
+
         const resultEl = document.createElement('ul');
+
         for (let i = 0; i < 10; i++) {
             const scoreEl = document.createElement('li');
             scoreEl.textContent = labels[i] + " " + scores[i];
             resultEl.appendChild(scoreEl);
             sampleHolderEl!.prepend(resultEl);
+
         }
+
     });
 };
 
