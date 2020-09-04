f19f93b08d1e7134c250f5cc6a5dd2ca1b9851a0
Jess Sullivan <Jess@sulliwood.org>
2020-09-04 13:50:29 -0400

    rm etc

--- etc/archive/convert.js
+++
@@ -1,8 +0,0 @@
-import * as tf from '@tensorflow/tfjs';
-import {loadGraphModel} from '@tensorflow/tfjs-converter';
-
-const MODEL_URL = 'model_directory/model.json';
-
-const model = await loadGraphModel(MODEL_URL);
-const cat = document.getElementById('cat');
-model.execute(tf.browser.fromPixels(cat));
