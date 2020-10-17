f726b3de0b74703650d2e1531bb6a8b44dfb035c
Jess <Jess@sulliwood.org>
2020-10-16 21:54:30 -0400

    spelling

--- tflite_demo_spec_input.py
+++ tflite_demo_spec_input.py
@@ -4,7 +4,7 @@ import numpy as np
 import tensorflow as tf
 
 # these operations are also used @ Classifier.classify_standard_proc() in classifier.py
-# line ~80 for the Flask endpoint on Herkoku.
+# (line ~80) for the Flask endpoint on Heroku (tmpui.herokuapp.com).
 
 # classification method using model built WITHOUT select ops-
 # requires all spectrogram matrix transformations occur outside of model
