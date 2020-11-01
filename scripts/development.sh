fc5c5423820d65e446144967df7d519185520637
Jess <Jess@sulliwood.org>
2020-10-31 23:49:46 -0400

    Update development.sh

--- scripts/development.sh
+++ scripts/development.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# echo "development: packing, this could take a while..."
-# webpack --config webpack/es6.demo.config.ts
+echo "development: packing, this could take a while..."
+webpack --config webpack/es6.demo.config.ts
 
-# echo -e "\n ...development: packing leaflet annotator tool...\n"
-# webpack --config webpack/webpack.anno_tool.js
+echo -e "\n ...development: packing leaflet annotator tool...\n"
+webpack --config webpack/webpack.anno_tool.js
 
 echo -e "\n ...development: packing annotator client...\n"
 webpack --config webpack/webpack.anno_client.ts
