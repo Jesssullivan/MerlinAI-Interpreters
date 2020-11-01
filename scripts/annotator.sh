723a858212cb69b2ba0791d2e9a99b07b777a811
Jess Sullivan <jess@sulliwood.org>
2020-10-31 23:14:25 -0400

    +- cleanup

--- scripts/annotator.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo -e "\ndevelopment: packing leaflet annotator tool...\n"
-webpack --config webpack/webpack.anno.js
-
-echo "development: packing done. \n opening demos/annotator.html..."
-google-chrome demos/annotator.html
