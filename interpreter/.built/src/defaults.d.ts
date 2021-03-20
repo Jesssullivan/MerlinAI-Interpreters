0b9cc8b31bb06341bdcaf8fa3f0db7758b161351
jesssullivan <Jess@sulliwood.org>
2021-03-19 20:58:43 -0400

    .

--- interpreter/.built/src/defaults.d.ts
+++
@@ -1,9 +0,0 @@
-declare const COLORS: string[];
-declare const BOX_PATH_STYLE: {
-    stroke: boolean;
-    weight: number;
-    opacity: number;
-    fill: boolean;
-    fillOpacity: number;
-};
-export { COLORS, BOX_PATH_STYLE };
--- interpreter/.built/src/image_info.d.ts
+++
@@ -1,6 +0,0 @@
-declare const React: any;
-export declare class ImageInfo extends React.Component {
-    constructor(props: null);
-    render(): any;
-}
-export {};
