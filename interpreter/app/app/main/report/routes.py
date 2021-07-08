4637c6100153d64abfb4127fdfc17502b838ef31
Jess <Jess@sulliwood.org>
2021-07-08 15:32:38 -0400

    Update routes.py

--- interpreter/app/app/main/report/routes.py
+++ interpreter/app/app/main/report/routes.py
@@ -7,6 +7,11 @@ reports_blueprint = Blueprint("reports", __name__)
 reports_blueprint.static_folder = "../../demos/"
 
 
+@reports_blueprint.route("chirping_birds", methods=["GET", "POST"])
+def esc_rd_chrp():
+    return reports_blueprint.send_static_file('chirping_birds.html')
+
+
 @reports_blueprint.route("media_icon.png/overview", methods=["GET", "POST"])
 def media_png():
     return app.send_static_file("media_icon.png")
@@ -15,11 +20,6 @@ def media_png():
 @reports_blueprint.route("/<sp>/overview", methods=["GET", "POST"])
 def rp_overview(sp):
     return reports_blueprint.send_static_file('html/' + sp + '.html')
-
-
-@reports_blueprint.route("chirping_birds", methods=["GET", "POST"])
-def esc_rd_chrp():
-    return reports_blueprint.send_static_file('chirping_birds.html')
 
 
 @reports_blueprint.route("/<sp>/<f>", methods=["GET", "POST"])
